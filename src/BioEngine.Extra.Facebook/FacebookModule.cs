262d2a133adf1c61dfe6edd2d7c1547bde211121
George <sonicgd@gmail.com>
2019-03-13 11:20:25 +0500

    Rework modules to use without host builder

--- src/BioEngine.Extra.Facebook/FacebookModule.cs
+++ src/BioEngine.Extra.Facebook/FacebookModule.cs
@@ -5,27 +5,28 @@ using BioEngine.Core.Modules;
 using BioEngine.Core.Properties;
 using BioEngine.Extra.Facebook.Service;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.Extra.Facebook
 {
     public class FacebookModule : BioEngineModule
     {
-        public override void ConfigureServices(WebHostBuilderContext builderContext, IServiceCollection services)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostingEnvironment environment)
         {
             services.Configure<FacebookServiceConfiguration>(config =>
             {
-                var parsed = Uri.TryCreate(builderContext.Configuration["BE_FACEBOOK_API_URL"], UriKind.Absolute,
+                var parsed = Uri.TryCreate(configuration["BE_FACEBOOK_API_URL"], UriKind.Absolute,
                     out var url);
                 if (!parsed)
                 {
                     throw new ArgumentException(
-                        $"Facebook api url is incorrect: {builderContext.Configuration["BE_FACEBOOK_API_URL"]}");
+                        $"Facebook api url is incorrect: {configuration["BE_FACEBOOK_API_URL"]}");
                 }
 
                 config.ApiUrl = url;
-                config.PageId = builderContext.Configuration["BE_FACEBOOK_PAGE_ID"];
-                config.AccessToken = builderContext.Configuration["BE_FACEBOOK_ACCESS_TOKEN"];
+                config.PageId = configuration["BE_FACEBOOK_PAGE_ID"];
+                config.AccessToken = configuration["BE_FACEBOOK_ACCESS_TOKEN"];
             });
             services.AddSingleton<FacebookService>();
             services.AddScoped<IRepositoryFilter, FacebookContentFilter>();
