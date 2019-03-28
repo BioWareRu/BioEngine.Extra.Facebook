e923a779fb7e4ae73e4df74eae9893adef0c6bc9
George <sonicgd@gmail.com>
2019-03-28 19:42:31 +0500

    Remove interfaces folder, move them to corresponding folders

--- src/BioEngine.Extra.Facebook/FacebookModule.cs
+++ src/BioEngine.Extra.Facebook/FacebookModule.cs
@@ -1,8 +1,8 @@
 using System;
 using BioEngine.Core.Entities;
-using BioEngine.Core.Interfaces;
 using BioEngine.Core.Modules;
 using BioEngine.Core.Properties;
+using BioEngine.Core.Repository;
 using BioEngine.Extra.Facebook.Service;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
