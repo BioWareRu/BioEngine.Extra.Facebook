f2b0499dd54dce19e36578e62532cac8f281dec6
George <sonicgd@gmail.com>
2019-05-10 21:34:05 +0500

    Start using nullable reference types from c# 8

--- src/BioEngine.Extra.Facebook/FacebookContentPropertiesSet.cs
+++ src/BioEngine.Extra.Facebook/FacebookContentPropertiesSet.cs
@@ -2,10 +2,10 @@ using BioEngine.Core.Properties;
 
 namespace BioEngine.Extra.Facebook
 {
-    [PropertiesSet(Name = "Публикация в Facebook", Quantity = PropertiesQuantity.OnePerSite)]
+    [PropertiesSet("Публикация в Facebook", Quantity = PropertiesQuantity.OnePerSite)]
     public class FacebookContentPropertiesSet : PropertiesSet
     {
-        [PropertiesElement(Name = "ID поста", Type = PropertyElementType.String)]
+        [PropertiesElement("ID поста")]
         public string PostId { get; set; }
     }
 }
--- src/BioEngine.Extra.Facebook/FacebookSitePropertiesSet.cs
+++ src/BioEngine.Extra.Facebook/FacebookSitePropertiesSet.cs
@@ -3,19 +3,18 @@ using BioEngine.Core.Properties;
 
 namespace BioEngine.Extra.Facebook
 {
-    [PropertiesSet(Name = "Публикации в Facebook", IsEditable = true)]
+    [PropertiesSet("Публикации в Facebook", IsEditable = true)]
     public class FacebookSitePropertiesSet : PropertiesSet
     {
-        [PropertiesElement(Name = "Включено?", Type = PropertyElementType.Checkbox)]
+        [PropertiesElement("Включено?", PropertyElementType.Checkbox)]
         public bool IsEnabled { get; set; }
 
-        [PropertiesElement(Name = "ID страницы", Type = PropertyElementType.String)]
-        public string PageId { get; set; }
+        [PropertiesElement("ID страницы")] public string PageId { get; set; }
 
-        [PropertiesElement(Name = "Токен", Type = PropertyElementType.PasswordString)]
+        [PropertiesElement("Токен", PropertyElementType.PasswordString)]
         public string AccessToken { get; set; }
 
-        [PropertiesElement(Name = "Адрес API", Type = PropertyElementType.Url)]
+        [PropertiesElement("Адрес API", PropertyElementType.Url)]
         public Uri ApiUrl { get; set; }
     }
 }
