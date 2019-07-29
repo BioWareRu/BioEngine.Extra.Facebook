eff943fdad781b3dcdc051b892494da02d24e3d8
George <sonicgd@gmail.com>
2019-07-29 10:30:32 +0500

    Drop title

--- src/BioEngine.Extra.Facebook/Entities/FacebookPublishRecord.cs
+++ src/BioEngine.Extra.Facebook/Entities/FacebookPublishRecord.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using BioEngine.Core.DB;
 using BioEngine.Core.Social;
 
@@ -8,7 +7,6 @@ namespace BioEngine.Extra.Facebook.Entities
     [Entity("facebookpublishrecord")]
     public class FacebookPublishRecord : BasePublishRecord
     {
-        [NotMapped] public override string Title { get; set; }
         [Required] public string PostId { get; set; }
     }
 }
